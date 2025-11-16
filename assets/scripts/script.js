764d65c48d72a97777b3483420399d93f7f357e2
BGNS <196232533+BGNS42@users.noreply.github.com>
2025-11-16 20:48:38 -0300

    game code and variables inside createJogo() constructor for security of game state variables

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,125 +5,127 @@
 // uma diagonal: (se ele tem arrays com todos os números iguais( [0,0], [1,1] ou [2,2]) ou a pontuação [2,0], [1,1] e [0,2]) 
 // em pé: (se ele tem todos os números da esquerda diferentes e da direita iguais, exemplo: [0,0], [1,0], [2,0])
 
-//const board = () => {}
-const gameBoard = {
-    board2: [[0,0], [0,1], [0,2],
-            [1,0], [1,1], [1,2],
-            [2,0], [2,1], [2,2]],
-    board: [1,2,3,
-            4,5,6,
-            7,8,9],
-
-    currentPlayer: "O",
-    escolhido: [],
-    escolhaP1: [],
-    escolhaP2: [],
-}
-
-const getRandomChoice = () => {
-    return Math.floor(Math.random() * 9) + 1;
-}
-
-const getP1 = function(player) {
-    gameBoard.escolhaP1.push(player);
-}
-
-const getP2 = function(player) {
-    gameBoard.escolhaP2.push(player);
-}
-
-const getEscolhidos = (player) => {
-    gameBoard.escolhido.push(player);
-    return gameBoard.escolhido;
-}
-
-
-const checkEscolhaP1 = () => {
-    let player = getRandomChoice();
-    if (!gameBoard.escolhido.includes(player)) {
-        getP1(player);
-        getEscolhidos(player);
-        console.log(`${gameBoard.currentPlayer}: ${player} /\n${gameBoard.currentPlayer} escolhidas: ${gameBoard.escolhaP1}\n`);
-    } else {
-        return false || checkEscolhaP1();
-    }
-}
-// console.log(gameBoard.escolhido);
-// gameBoard.escolhido.push(1,2,3,4);
-// console.log(gameBoard.escolhido);
-
-const checkEscolhaP2 = () => {
-    let player = getRandomChoice();
-    if (!gameBoard.escolhido.includes(player)) {
-        getP2(player);
-        getEscolhidos(player);
-        console.log(`${gameBoard.currentPlayer}: ${player} /\n${gameBoard.currentPlayer} escolhidas: ${gameBoard.escolhaP2}\n`);
-    } else {
-        return false || checkEscolhaP2();
-    }
-}
-
-const getEscolha = () => {
-    if (gameBoard.currentPlayer === "X") {
-        gameBoard.currentPlayer = "O";
-    } else {
-        gameBoard.currentPlayer = "X";
-    }
-
-    switch (gameBoard.currentPlayer) {
-        case "X":
-            checkEscolhaP1();
-            break;
-        case "O":
-            checkEscolhaP2();
-            break;
-        default:
-            console.log("Erro, nem X nem O");
-            break;
-    }
-}
-
-function checkWin() {
-    const winCombos = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
-        [1,4,7],
-        [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,1],
-    ];
-
-    const player1HasWon = winCombos.some(combo => {
-        return combo.every(cell => gameBoard.escolhaP1.includes(cell));
-    });
-    const player2HasWon = winCombos.some(combo => {
-        return combo.every(cell => gameBoard.escolhaP2.includes(cell));
-    });
-
-    if (player1HasWon) {
-        console.log(`\n${gameBoard.currentPlayer} venceu!\n`);
-        return true;
-    } 
+const createJogo = () => {
+    const gameBoard = {
+        // board2: [[0,0], [0,1], [0,2],
+        //         [1,0], [1,1], [1,2],
+        //         [2,0], [2,1], [2,2]],
+        board: [1,2,3,
+                4,5,6,
+                7,8,9],
     
-    if (player2HasWon){
-        console.log(`\n${gameBoard.currentPlayer} venceu!\n`);
-        return true;
-    }     
+        currentPlayer: "O",
+        escolhido: [],
+        escolhaP1: [],
+        escolhaP2: [],
+    }
+    
+    const getRandomChoice = () => {
+        return Math.floor(Math.random() * 9) + 1;
+    }
+    
+    const getP1 = function(player) {
+        gameBoard.escolhaP1.push(player);
+    }
+    
+    const getP2 = function(player) {
+        gameBoard.escolhaP2.push(player);
+    }
+    
+    const getEscolhidos = (player) => {
+        gameBoard.escolhido.push(player);
+        return gameBoard.escolhido;
+    }
 
-    if (gameBoard.escolhido.length === 9) {
-        console.log("\nEmpate\n");
-        return true;
-    } 
-    return false;
-}
+    const checkEscolhaP1 = () => {
+        let player = getRandomChoice();
+        if (!gameBoard.escolhido.includes(player)) {
+            getP1(player);
+            getEscolhidos(player);
+            console.log(`${gameBoard.currentPlayer}: ${player} /\n${gameBoard.currentPlayer} escolhidas: ${gameBoard.escolhaP1}\n`);
+        } else {
+            return false || checkEscolhaP1();
+        }
+    }
+
+    const checkEscolhaP2 = () => {
+        let player = getRandomChoice();
+        if (!gameBoard.escolhido.includes(player)) {
+            getP2(player);
+            getEscolhidos(player);
+            console.log(`${gameBoard.currentPlayer}: ${player} /\n${gameBoard.currentPlayer} escolhidas: ${gameBoard.escolhaP2}\n`);
+        } else {
+            return false || checkEscolhaP2();
+        }
+    }
+
+    const getEscolha = () => {
+        if (gameBoard.currentPlayer === "X") {
+            gameBoard.currentPlayer = "O";
+        } else {
+            gameBoard.currentPlayer = "X";
+        }
+    
+        switch (gameBoard.currentPlayer) {
+            case "X":
+                checkEscolhaP1();
+                break;
+            case "O":
+                checkEscolhaP2();
+                break;
+            default:
+                console.log("Erro, nem X nem O");
+                break;
+        }
+    }
+
+    const checkWin = function () {
+        const winCombos = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [1,4,7],
+            [2,5,8],
+            [3,6,9],
+            [1,5,9],
+            [3,5,1],
+        ];
+    
+        const player1HasWon = winCombos.some(combo => {
+            return combo.every(cell => gameBoard.escolhaP1.includes(cell));
+        });
+        const player2HasWon = winCombos.some(combo => {
+            return combo.every(cell => gameBoard.escolhaP2.includes(cell));
+        });
+    
+        if (player1HasWon) {
+            console.log(`\n${gameBoard.currentPlayer} venceu!\n`);
+            return true;
+        } 
+        
+        if (player2HasWon){
+            console.log(`\n${gameBoard.currentPlayer} venceu!\n`);
+            return true;
+        }     
+    
+        if (gameBoard.escolhido.length === 9) {
+            console.log("\nEmpate\n");
+            return true;
+        } 
+        return false;
+    }
+
+    const getGameBoard = () => gameBoard.escolhido;
+
+    return { getEscolha, checkWin, getGameBoard };
+};
 
 const jogo = (() => {
     //let end = false;
+    const ticTacToe = createJogo();
     while (!end) {
-        getEscolha();
-        var end = checkWin();
+        ticTacToe.getEscolha();
+        var end = ticTacToe.checkWin();
     }
-    console.log(gameBoard.escolhido)
+    console.log(ticTacToe.getGameBoard())
 })();
