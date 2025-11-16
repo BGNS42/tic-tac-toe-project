bd87e1a0f4a12014c55c5ff4e54996284611cafa
BGNS <196232533+BGNS42@users.noreply.github.com>
2025-11-16 20:24:52 -0300

    correct a bug that do not check the array properly to checkWin()

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,7 @@
 // uma diagonal: (se ele tem arrays com todos os números iguais( [0,0], [1,1] ou [2,2]) ou a pontuação [2,0], [1,1] e [0,2]) 
 // em pé: (se ele tem todos os números da esquerda diferentes e da direita iguais, exemplo: [0,0], [1,0], [2,0])
 
+//const board = () => {}
 const gameBoard = {
     board2: [[0,0], [0,1], [0,2],
             [1,0], [1,1], [1,2],
@@ -12,18 +13,13 @@ const gameBoard = {
     board: [1,2,3,
             4,5,6,
             7,8,9],
-    
-    //winCon: ,
+
     currentPlayer: "O",
     escolhido: [],
     escolhaP1: [],
     escolhaP2: [],
-    escolheu: [[],[]],
 }
 
-// const getRandomChoice = () => {
-//     return [Math.floor((Math.random() * 9)), Math.floor((Math.random() * 9))];
-// }
 const getRandomChoice = () => {
     return Math.floor(Math.random() * 9) + 1;
 }
@@ -43,11 +39,11 @@ const getEscolhidos = (player) => {
 
 
 const checkEscolhaP1 = () => {
-    let player1 = getRandomChoice();
-    if (!gameBoard.escolhido.includes(player1)) {
-        getP1(player1);
-        getEscolhidos(player1);
-        console.log(`P1: ${player1}/ p1 escolhidas: ${gameBoard.escolhaP1}`);
+    let player = getRandomChoice();
+    if (!gameBoard.escolhido.includes(player)) {
+        getP1(player);
+        getEscolhidos(player);
+        console.log(`${gameBoard.currentPlayer}: ${player} /\n${gameBoard.currentPlayer} escolhidas: ${gameBoard.escolhaP1}\n`);
     } else {
         return false || checkEscolhaP1();
     }
@@ -57,11 +53,11 @@ const checkEscolhaP1 = () => {
 // console.log(gameBoard.escolhido);
 
 const checkEscolhaP2 = () => {
-    let player2 = getRandomChoice();
-    if (!gameBoard.escolhido.includes(player2)) {
-        getP2(player2);
-        getEscolhidos(player2);
-        console.log(`P2: ${player2}/ p2 escolhidas: ${gameBoard.escolhaP2}`);
+    let player = getRandomChoice();
+    if (!gameBoard.escolhido.includes(player)) {
+        getP2(player);
+        getEscolhidos(player);
+        console.log(`${gameBoard.currentPlayer}: ${player} /\n${gameBoard.currentPlayer} escolhidas: ${gameBoard.escolhaP2}\n`);
     } else {
         return false || checkEscolhaP2();
     }
@@ -70,12 +66,10 @@ const checkEscolhaP2 = () => {
 const getEscolha = () => {
     if (gameBoard.currentPlayer === "X") {
         gameBoard.currentPlayer = "O";
-        console.log(gameBoard.currentPlayer);
     } else {
         gameBoard.currentPlayer = "X";
-        console.log(gameBoard.currentPlayer);
     }
-    
+
     switch (gameBoard.currentPlayer) {
         case "X":
             checkEscolhaP1();
@@ -89,71 +83,47 @@ const getEscolha = () => {
     }
 }
 
-// function containsArray(player) {
-//     // if (gameBoard.escolhido.indexOf(player) !== -1) {
-//     //     return true
-//     // } else {
-//     //     return false
-//     // }
-//     return gameBoard.escolhido.some(innerArr => {
-//         if(innerArr.length !== player.length) {
-//             return false;
-//         }
-//         return innerArr.every((value, index) => value === player[index]);
-//     });
-// }
-
-// function containsArrayP1(player) {
-//     return gameBoard.escolhaP1.some(innerArr => {
-//         if(innerArr.length !== player.length) {
-//             return false;
-//         }
-//         return innerArr.every((value, index) => value === player[index]);
-//     });
-// }
-
-// function containsArrayP2(player) {
-//     return gameBoard.escolhaP2.some(innerArr => {
-//         if(innerArr.length !== player.length) {
-//             return false;
-//         }
-//         return innerArr.every((value, index) => value === player[index]);
-//     });
-// }
-
 function checkWin() {
-    if ((gameBoard.escolhaP1.includes(1,2,3))||(gameBoard.escolhaP1.includes(1,4,7))||(gameBoard.escolhaP1.includes(4,5,6))||(gameBoard.escolhaP1.includes(2,5,8))||(gameBoard.escolhaP1.includes(7,8,9))||(gameBoard.escolhaP1.includes(3,6,9))||(gameBoard.escolhaP1.includes(1,5,9))||(gameBoard.escolhaP1.includes(3,5,7))) {
-        console.log("\nPlayer 1 venceu!\n");
+    const winCombos = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,1],
+    ];
+
+    const player1HasWon = winCombos.some(combo => {
+        return combo.every(cell => gameBoard.escolhaP1.includes(cell));
+    });
+    const player2HasWon = winCombos.some(combo => {
+        return combo.every(cell => gameBoard.escolhaP2.includes(cell));
+    });
+
+    if (player1HasWon) {
+        console.log(`\n${gameBoard.currentPlayer} venceu!\n`);
         return true;
-    } else if (gameBoard.escolhido.length === 9) {
-        console.log("\nEmpate\n");
-        return true;
-    } else if ((gameBoard.escolhaP2.includes(1,2,3))||(gameBoard.escolhaP2.includes(1,4,7))||(gameBoard.escolhaP2.includes(4,5,6))||(gameBoard.escolhaP2.includes(2,5,8))||(gameBoard.escolhaP2.includes(7,8,9))||(gameBoard.escolhaP2.includes(3,6,9))||(gameBoard.escolhaP2.includes(1,5,9))||(gameBoard.escolhaP2.includes(3,5,7))){
-        console.log("\nPlayer 2 venceu!\n");
+    } 
+    
+    if (player2HasWon){
+        console.log(`\n${gameBoard.currentPlayer} venceu!\n`);
         return true;
     }     
+
+    if (gameBoard.escolhido.length === 9) {
+        console.log("\nEmpate\n");
+        return true;
+    } 
     return false;
-//     if ((containsArrayP1(1) && containsArrayP1(2) && containsArrayP1(3))||(containsArrayP1(1) && containsArrayP1(4) && containsArrayP1(7))||(containsArrayP1(4) && containsArrayP1(5) && containsArrayP1(6))||(containsArrayP1(2) && containsArrayP1(5) && containsArrayP1(8))||(containsArrayP1(7) && containsArrayP1(8) && containsArrayP1(9))||(containsArrayP1(3) && containsArrayP1(6) && containsArrayP1(9))||(containsArrayP1(1) && containsArrayP1(5) && containsArrayP1(9))||(containsArrayP1(3) && containsArrayP1(5) && containsArrayP1(7))) {
-//         console.log("\nPlayer 1 venceu!\n");
-//         return true;
-//     } else if (gameBoard.escolhido.length === 9) {
-//         console.log("\nEmpate\n");
-//         return true;
-//     } else if ((containsArrayP2(1) && containsArrayP2(2) && containsArrayP2(3))||(containsArrayP2(1) && containsArrayP2(4) && containsArrayP2(7))||(containsArrayP2(4) && containsArrayP2(5) && containsArrayP2(6))||(containsArrayP2(2) && containsArrayP2(5) && containsArrayP2(8))||(containsArrayP2(7) && containsArrayP2(8) && containsArrayP2(9))||(containsArrayP2(3) && containsArrayP2(6) && containsArrayP2(9))||(containsArrayP2(1) && containsArrayP2(5) && containsArrayP2(9))||(containsArrayP2(3) && containsArrayP2(5) && containsArrayP2(7))){
-//         console.log("\nPlayer 2 venceu!\n");
-//         return true;
-//     } else {
-//         return false;
-//     }
 }
 
-//const winCon = 
 const jogo = (() => {
-    let end = false;
+    //let end = false;
     while (!end) {
-        
-        
         getEscolha();
-        end = checkWin();
+        var end = checkWin();
     }
+    console.log(gameBoard.escolhido)
 })();
